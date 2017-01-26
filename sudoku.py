1ebc16c964c8497e1979f6bd9ce8b633e0b60f10
Luzi82 <luzi82@gmail.com>
2017-01-26 22:48:43 +0800

    implement v-transform

--- sudoku.py
+++ sudoku.py
@@ -12,6 +12,21 @@ def w_transform_reverse(nparray):
     nparray = np.reshape(nparray,[9,9,9])
     return nparray
 
+def v_transform(nparray):
+    nparray = np.reshape(nparray,[3,3,9,9])
+    nparray = np.any(nparray,axis=1)
+    nparray = np.moveaxis(nparray,range(3),[2,1,0])
+    nparray = np.reshape(nparray,[27,3,3])
+    return nparray
+
+def v_transform_reverse(nparray):
+    nparray = np.reshape(nparray,[9,9,3])
+    nparray = np.moveaxis(nparray,range(3),[2,1,0])
+    nparray = np.reshape(nparray,[3,1,9,9])
+    nparray = np.repeat(nparray,3,axis=1)
+    nparray = np.reshape(nparray,[9,9,9])
+    return nparray
+
 if __name__ == '__main__':
     import unittest
     import utils
@@ -43,12 +58,56 @@ if __name__ == '__main__':
             self.assertEqual(nparray0[7][1][0],False)
             self.assertEqual(nparray0[7][1][1],True)
 
-        def test_w_transform(self):
+        def test_w_transform_reverse(self):
             diagonal_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
             values = utils.grid_values(diagonal_grid)
             nparray = sudoku_convert.value_to_nparray(values)
             nparray0 = w_transform(nparray)
             nparray1 = w_transform_reverse(nparray0)
             self.assertEqual(np.logical_xor(nparray, nparray1).any(),False)
+
+        def test_v_transform(self):
+            '''
+            .........
+            .........
+            .........
+            .........
+            .........
+            .........
+            ...987...
+            ...965...
+            ...9.3...
+            '''
+            diagonal_grid = '.........................................................987......965......9.3...'
+            values = utils.grid_values(diagonal_grid)
+            nparray = sudoku_convert.value_to_nparray(values)
+            nparray0 = v_transform(nparray)
+            self.assertEqual(nparray0.shape,(27,3,3))
+            nparray1 = np.reshape(nparray0,[9,3,3,3])
+            self.assertEqual(nparray1[8][1][0][2],True)
+            self.assertEqual(nparray1[7][1][0][2],False)
+            self.assertEqual(nparray1[0][1][1][2],True)
+            self.assertEqual(nparray1[7][1][1][2],True)
+            self.assertEqual(nparray1[0][1][2][2],False)
+            self.assertEqual(nparray1[1][1][2][2],False)
+            self.assertEqual(nparray1[2][1][2][2],True)
+            self.assertEqual(nparray1[4][1][2][2],True)
+            self.assertEqual(nparray1[6][1][2][2],True)
+
+        def test_v_transform_reverse(self):
+            diagonal_grid = '.........................................................987......965......9.3...'
+            values = utils.grid_values(diagonal_grid)
+            nparray = sudoku_convert.value_to_nparray(values)
+            nparray0 = v_transform(nparray)
+            nparray1 = v_transform_reverse(nparray0)
+            self.assertEqual(nparray1[6][3][0],False)
+            self.assertEqual(nparray1[6][3][8],True)
+            self.assertEqual(nparray1[6][4][0],True)
+            self.assertEqual(nparray1[6][4][8],True)
+            self.assertEqual(nparray1[8][4][0],True)
+            self.assertEqual(nparray1[8][4][8],True)
+            self.assertEqual(nparray1[7][5][5],False)
+            self.assertEqual(nparray1[7][5][6],True)
+            self.assertEqual(nparray1[7][5][7],False)
 
     unittest.main()
